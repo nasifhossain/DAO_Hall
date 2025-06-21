85f212af453e8a46e6b25d4af69971e56d7e8185
Nasif <Nasif>
2025-06-22 00:42:57 +0530

    port fix

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const http = require("http");
 const app = require("./app");
-const PORT = 3000;
-
+require("dotenv").config();
 const server = http.createServer(app);
 server.listen(PORT,()=>{
-    console.log(`App is running on port ${PORT}`);
+    console.log(`App is running on port ${process.env.PORT || 3000}`);
     
 })
